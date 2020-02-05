66ffe44c56ce4edd98b9c52501c38ef8c49f5d66
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-02-05 15:54:54 -0300

    Criei a array para armazenar os posts

--- semana4/classes/blog/index.js
+++ semana4/classes/blog/index.js
@@ -1,23 +1,21 @@
-class Post {
+class Post{
     constructor(titulo, autor, conteudo) {
-
         this.titulo = titulo
         this.autor = autor
         this.conteudo = conteudo
     }
 }
 
+var postagens = []
 
 function novoPost(){
-    
     let titulo = document.getElementById("titulo").value
-    let autor = document.getElementById("autor").value 
-    let conteudo= document.getElementById("conteudo").value
+    let autor = document.getElementById("autor").value
+    let conteudo = document.getElementById("conteudo").value
 
     let post = new Post(titulo, autor, conteudo)
 
-    console.log(post)
+    postagens.push(post)
 
-    
-
+    console.log(postagens)
 }
