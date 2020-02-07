95ec6c8f49a8d6537ad97c0674fb8ce454a596cb
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-02-07 15:47:57 -0300

    Exercícios de Interpretação

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,41 @@
+//Exercícios de Interpretação
+
+/*Exercício 1
+
+A função está recebendo um valor por parâmetro, que seria um valor em dólar. A função pede pro usuário informar o valor da cotação do Dólar e retorna
+o valor em reais. Abaixo da função é criada uma variável "meuDinheiro" e invocamos a função nessa variável passando um parâmetro "100". Supondo que a cotação do Dólar
+esteja em R$ 4.00, a função vai receber o valor 100(dólares) por parâmetro e calcular o valor em reais escrevendo no console "R$ 400.00".
+
+*/
+
+/*Exercício 2 
+
+A função recebe 2 parâmetros(tipoDeInvestimento e valor), após receber esses 2 parâmetros é verificado através de um switch/case qual é o tipo de investimento e quanto ira render
+com o valor recebido. Criando duas váriaveis e invocando a função, com os parâmetros da primeira váriavel ("Ações", 150) e da segunda váriavel ("Tesouro", 150) o resultado no console 
+seria (165) e "TIPO DE INVESTIMENTO INFORMADO INCORRETO!" respectivamente.
+
+*/
+
+/*Exercício 3 
+
+Temos 3 arrays declarados numeros[], array1[] e array2[], o for está percorrendo o array numeros[] e verificando quais números são pares e quais são ímpares, os números pares são adicionados
+através do método "push" no array1[] e os ímpares no array2[].
+No console teríamos o seguinte resultado:
+Quantidade total de números 14
+6
+8
+
+*/
+
+/*Exercício 4
+
+Temo um array de números e 2 variáveis a primeira declarada como Infinity(Pesquisei apenas o valor de Infity, para saber que são números positivos) e a segundo como 0. O loop for está 
+percorrendo o array de números, dentro do for temos 2 if, um verifica se numero do array é menor que a variável declarada como Infitnity e se for menor a variável vai receber esse valor.
+O segundo if verifica se o numero do array é maior que a segunda variável declarada inicialmente como 0, se for maior o valor da variável recebe o valor do número.
+
+No console teríamos o seguinte resultado:
+
+-10
+1590
+
+*/
