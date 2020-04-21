e39d59d8cd278e11c57eac61585e6b813043def2
Galanzito <41401599+Galanzito@users.noreply.github.com>
2020-04-21 20:57:13 -0300

    Resolução Exercicio 4

--- semana14/introducao_typescript/firstScript/build/exercicio4.js
+++ semana14/introducao_typescript/firstScript/build/exercicio4.js
@@ -1,22 +1,39 @@
-function periodosDaHistoria(ano, sigla) {
-    if (ano < 4000 && sigla === "AC") {
-        return "Pré-história";
-    }
-    else if (ano > 4000 && sigla === "AC") {
-        return "Idade Antiga";
-    }
-    else if (ano >= 476 && ano < 1453 && sigla === "DC") {
-        return "Idade Média";
-    }
-    else if (ano >= 1453 && ano < 1789 && sigla === "DC") {
-        return "Idade Moderna";
-    }
-    else if (ano > 1789 && sigla === "DC") {
-        return "Idade Contemporânea";
+var IDADES_HISTORICAS;
+(function (IDADES_HISTORICAS) {
+    IDADES_HISTORICAS["PRE_HISTORIA"] = "Pr\u00E9-hist\u00F3ria";
+    IDADES_HISTORICAS["IDADE_ANTIGA"] = "Idade Antiga";
+    IDADES_HISTORICAS["IDADE_MEDIA"] = "Idade M\u00E9dia";
+    IDADES_HISTORICAS["IDADE_MODERNA"] = "Idade Moderna";
+    IDADES_HISTORICAS["IDADE_CONTEMPORANEA"] = "idade Contempor\u00E2nea";
+    IDADES_HISTORICAS["PERIODO_INVALIDO"] = "Este Per\u00EDdo n\u00E3o existe!";
+})(IDADES_HISTORICAS || (IDADES_HISTORICAS = {}));
+var ANTES_DEPOIS_CRISTO;
+(function (ANTES_DEPOIS_CRISTO) {
+    ANTES_DEPOIS_CRISTO["AC"] = "Antes de Cristo";
+    ANTES_DEPOIS_CRISTO["DC"] = "Depois de Cristo";
+})(ANTES_DEPOIS_CRISTO || (ANTES_DEPOIS_CRISTO = {}));
+function encontaIdadeHistoria(ano, ACDC) {
+    ACDC = ACDC || ANTES_DEPOIS_CRISTO.DC;
+    if (ACDC === ANTES_DEPOIS_CRISTO.AC) {
+        if (ano > 4000) {
+            return IDADES_HISTORICAS.PRE_HISTORIA;
+        }
+        else {
+            return IDADES_HISTORICAS.IDADE_ANTIGA;
+        }
     }
     else {
-        return "Algo deu errado";
+        if (ano < 476) {
+            return IDADES_HISTORICAS.IDADE_MEDIA;
+        }
+        else if (ano < 1453) {
+            return IDADES_HISTORICAS.IDADE_MODERNA;
+        }
+        else {
+            return IDADES_HISTORICAS.IDADE_CONTEMPORANEA;
+        }
     }
+    return IDADES_HISTORICAS.PERIODO_INVALIDO;
 }
-console.log(periodosDaHistoria(4000, "AC"));
+console.log(encontaIdadeHistoria(4500, ANTES_DEPOIS_CRISTO.AC));
 //# sourceMappingURL=exercicio4.js.map
--- semana14/introducao_typescript/firstScript/src/exercicio4.ts
+++ semana14/introducao_typescript/firstScript/src/exercicio4.ts
@@ -1,17 +1,35 @@
-function periodosDaHistoria(ano: number, sigla: string): string{
-    if(ano < 4000 && sigla === "AC"){
-        return "Pré-história"
-    }else if(ano > 4000 && sigla === "AC"){
-        return "Idade Antiga"
-    }else if(ano >= 476 && ano < 1453 && sigla === "DC"){
-        return "Idade Média"
-    }else if(ano >= 1453 && ano < 1789 && sigla === "DC"){
-        return "Idade Moderna"
-    }else if(ano > 1789 && sigla === "DC"){
-        return "Idade Contemporânea"
-    }else{
-        return "Algo deu errado"
-    }
+enum IDADES_HISTORICAS {
+    PRE_HISTORIA = "Pré-história",
+    IDADE_ANTIGA = "Idade Antiga",
+    IDADE_MEDIA = "Idade Média",
+    IDADE_MODERNA = "Idade Moderna",
+    IDADE_CONTEMPORANEA = "idade Contemporânea",
+    PERIODO_INVALIDO = "Este Perído não existe!"
 }
 
-console.log(periodosDaHistoria(4000, "AC"))
+enum ANTES_DEPOIS_CRISTO {
+    AC = "Antes de Cristo",
+    DC = "Depois de Cristo"
+}
+
+function encontaIdadeHistoria(ano: number, ACDC?: ANTES_DEPOIS_CRISTO): IDADES_HISTORICAS{
+    ACDC = ACDC || ANTES_DEPOIS_CRISTO.DC
+    if(ACDC === ANTES_DEPOIS_CRISTO.AC){
+        if(ano>4000){
+            return IDADES_HISTORICAS.PRE_HISTORIA
+        }else{
+            return IDADES_HISTORICAS.IDADE_ANTIGA
+        }
+    }else{
+        if(ano<476){
+            return IDADES_HISTORICAS.IDADE_MEDIA
+        }else if(ano<1453){
+            return IDADES_HISTORICAS.IDADE_MODERNA
+        }else{
+            return IDADES_HISTORICAS.IDADE_CONTEMPORANEA
+        }
+    }
+    return IDADES_HISTORICAS.PERIODO_INVALIDO
+}
+
+console.log(encontaIdadeHistoria(4500, ANTES_DEPOIS_CRISTO.AC))
